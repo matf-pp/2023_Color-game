f453884df983f282ca2e8ad479d8f6aaf7bec08b
StefanJevtic63 <stefanjevtic10@yahoo.com>
2023-04-28 20:23:25 +0200

    game2 update: dodati dogadjaji

--- scenes/game2.lua
+++ scenes/game2.lua
@@ -5,9 +5,20 @@ local relayout = require("libs.relayout")
 local utilities = require("classes.utilities")
 
 -- Set variables
+score = 0
+math.randomseed(133)
 
 --Layout
 local _W, _H, _CX, _CY = relayout._W, relayout._H, relayout._CX, relayout._CY
+local background
+local duration = 5
+
+local theme = utilities:checkBackground()
+if utilities:checkBackground() == "white" then
+    theme = {1, 1, 1}
+else
+    theme = {0, 0, 0}
+end
 
 --Scene
 local scene = composer.newScene()
@@ -19,8 +30,7 @@ local _grpMain
 local _click = audio.loadStream("assets/sounds/click.wav")
 
 --Boje
-local colors = {"crvena","zelena", "plava","zuta", "ljubicasta", "braon", "narandzasta", "bela", "crna"}
-
+local colors = {"crvena", "zelena", "plava", "zuta", "ljubicasta", "braon", "narandzasta", "bela", "crna"}
 
 local clrRed =    {1, 51/255, 51/255}       
 local clrGreen =  {102/255, 204/255, 0}     
@@ -34,55 +44,207 @@ local clrBlack =  {0, 0, 0}
 
 local colorsArray = {clrRed, clrGreen, clrBlue, clrYellow, clrPurple, clrBrown, clrOrange, clrWhite, clrBlack}
 
+local _lblTapToStart = ""
+local start_time = system.getTimer()
+local progress_bar
+
+
 -- Local functions
+local function gotoWrongChoice()
+  utilities:playSound(_click) 
+  composer.gotoScene("scenes.wrongChoice")
+  _grpMain = display.newGroup()
+end
+
+
+local function getTrueColorName(color)
+  local name
+  if(color == clrRed) then
+    name = "crvena"
+  elseif(color == clrGreen) then
+    name = "zelena"
+  elseif(color == clrBlue) then
+    name = "plava"
+  elseif(color == clrYellow) then
+    name = "zuta"
+  elseif(color == clrPurple) then
+    name = "ljubicasta"
+  elseif(color == clrBrown) then
+    name = "braon"
+  elseif(color == clrOrange) then
+    name = "narandzasta"
+  elseif(color == clrWhite) then
+    name = "bela"
+  elseif(color == clrBlack) then
+    name = "crna"
+  end
+
+  return name
+end
+
+local function getRandomColors(t)
+  if ( type(t) ~= "table" ) then
+    print( "WARNING: getRandomColors function expects two tables" )
+    return nil, nil, nil, nil
+  end
+
+  local randomNumber1 = math.random(1, table.getn(t)-1)
+  local randomColor1 = t[randomNumber1]
+  table.remove(t, randomNumber1)
+
+  local randomNumber2 = math.random(1, table.getn(t)-1)
+  local randomColor2 = t[randomNumber2]
+  table.remove(t, randomNumber2)
+
+  local randomNumber3 = math.random(1, table.getn(t)-1)
+  local randomColor3 = t[randomNumber3]
+  table.remove(t, randomNumber3)
+
+  return randomColor1, randomColor2, randomColor3
+end
+
+local function calculate_progress()   -- as a percentage
+  return (system.getTimer() - start_time) / duration
+end
+
+local function update_progress_bar(time_passed, total_time)
+  local progress = time_passed / total_time
+  progress_bar.width = progress * 2*display.contentWidth
+  progress_bar.x = 0
+end
+
 local function SP2()
   local tablesGroup = display.newGroup()
+  _lblTapToStart.alpha = 0
+
+  local start_time = os.time() -- record the start time
+  local total_time = 5 -- set the total time for the timer in seconds
+  local elapsed_time = 0 -- initialize the elapsed time to zero
+
+  local function on_enter_frame()
+    local elapsed_time = os.time() - start_time -- calculate the elapsed time
+    if elapsed_time >= total_time then
+        -- the timer is finished
+        print("Timer complete!")
+        progress_bar.width = 0 -- set the progress bar width to 0
+        Runtime:removeEventListener("enterFrame", on_enter_frame)
+    else
+        local progress = elapsed_time / total_time -- calculate progress as a fraction between 0 and 1
+        local max_width = 2*display.contentWidth + 200  -- set the maximum width of the progress bar
+        local min_width = 0 -- set the minimum width of the progress bar
+
+        -- calculate the current width of the progress bar as a function of the progress
+        local current_width = max_width - (max_width - min_width) * progress
+
+        -- update the width of the progress bar
+        progress_bar.width = current_width
+    end
+  end
+
+  Runtime:addEventListener("enterFrame", on_enter_frame)
 
   --nasumicno izaberi boju
   local randomNumber1 = math.random(1,table.getn(colors)-1)
   local randomColor = colors[randomNumber1]
   local randomNumber2 = math.random(1,table.getn(colorsArray)-1)
 
-  --promeni boju pozadine
-  -- local background = display.newImageRect(_grpMain, "ColorUpAssets/assets/images/Gray-background.jpeg", _W, _H)
-  -- background.x = _CX
-  -- background.y = _CY
-  -- background.alpha = 0.9
+  --podaci o pozicijama labela
+  X_main_color = 0
+  Y_main_color = 0
+
+  X_option_1_3 = -80
+  X_option_2_4 = 80
+  Y_option_1_2 = 150
+  Y_option_3_4 = 250
+
+  color_width = 140
+  color_height = 60
+
+  cornerRadius = 5
 
   --prikazi boju koju korisnik treba izabrati medju ponudjenim
-  local tableRect = display.newRect(tablesGroup, 150, 40, 180, 80)
-  tableRect:setFillColor(0.5, 0.5, 0.5)
-  local tableText = display.newText(tablesGroup, colors[7], 150, 40, native.systemFont, 24)
-  local fillStr = colorsArray[randomNumber2]
-  tableText:setFillColor(fillStr[1], fillStr[2], fillStr[3])
+  local rectMainColor = display.newRoundedRect(tablesGroup, X_main_color, Y_main_color, color_width, color_height, cornerRadius)
+  rectMainColor.fill = theme
+  local textMainColor = display.newText(tablesGroup, randomColor, X_main_color, Y_main_color, native.systemFont, 24)
+  local trueColor = colorsArray[randomNumber2]
+  textMainColor:setFillColor(trueColor[1], trueColor[2], trueColor[3])
+
+
+  --pravimo kopiju niza boja iz koje cemo ukloniti glavnu boju kako je ne bi kasnije izabrali dva puta
+  local colorsCopy = table.copy(colors)
+  local trueColorName = getTrueColorName(trueColor)
+  local index = table.indexOf(colorsCopy, trueColorName)
+  table.remove(colorsCopy, index)
+
+  --izaberi 3 nasumicne boje iz pomocnog niza, i svaki put izbaci odabranu boju (kako se ne bi ponovila)
+  local randomColor1, randomColor2, randomColor3 = getRandomColors(colorsCopy)
+
+  --dodaj ove 4 opcije u novu tabelu i nasumicno izvuci 4 opcije (da ne bi neke od opcija bile fiksne)
+  local colorsOptions = {randomColor1, randomColor2, randomColor3, trueColorName}
+  local randomColor1, randomColor2, randomColor3 = getRandomColors(colorsOptions)
+  local randomColor4 = colorsOptions[1]
 
 
   --prikazi cetiri ponudjene boje od kojih je jedna tacna
-  --1
-  local tableRect = display.newRect(tablesGroup, 50, 300, 120, 80)
-  tableRect:setFillColor(0.5, 0.5, 0.5)
-  local tableText = display.newText(tablesGroup, colors[1], 50, 300, native.systemFont, 24)
-  tableText:setFillColor(1, 1, 1)
+  local function showOptions(number)
+    local X_pos, Y_pos, randomColorNum
 
-  --2 fiksiramo pravu boju na drugo mesto, pri cemu postoji mogucnost da je neka od ostalih opcija pokupi
-  local tableRect = display.newRect(tablesGroup, 210, 300, 120, 80)
-  tableRect:setFillColor(0.5, 0.5, 0.5)
-  local tableText = display.newText(tablesGroup, randomColor, 210, 300, native.systemFont, 24)
-  tableText:setFillColor(1, 1, 1)
+    if(number == 1) then
+      X_pos = X_option_1_3
+      Y_pos = Y_option_1_2
+      randomColorNum = randomColor1
+    elseif(number == 2) then
+      X_pos = X_option_2_4
+      Y_pos = Y_option_1_2
+      randomColorNum = randomColor2
+    elseif(number == 3) then
+      X_pos = X_option_1_3
+      Y_pos = Y_option_3_4
+      randomColorNum = randomColor3
+    elseif(number == 4) then
+      X_pos = X_option_2_4
+      Y_pos = Y_option_3_4
+      randomColorNum = randomColor4
+    end
 
-  --3
-  local tableRect = display.newRect(tablesGroup, 50, 400, 120, 80)
-  tableRect:setFillColor(0.5, 0.5, 0.5)
-  local tableText = display.newText(tablesGroup, colors[3], 50, 400, native.systemFont, 24)
-  tableText:setFillColor(1, 1, 1)
+    local rectOption = display.newRoundedRect(tablesGroup, X_pos, Y_pos, color_width, color_height, cornerRadius)
+    rectOption.fill = theme
+    rectOption.alpha = 0.9
+    local textOption = display.newText(tablesGroup, randomColorNum, X_pos, Y_pos, native.systemFont, 24)
+    textOption:setFillColor(1, 1, 1)
 
-  --4
-  local tableRect = display.newRect(tablesGroup, 210, 400, 120, 80)
-  tableRect:setFillColor(0.5, 0.5, 0.5)
-  local tableText = display.newText(tablesGroup, colors[4], 210, 400, native.systemFont, 24)
-  tableText:setFillColor(1, 1, 1)
+    return rectOption
+  end
 
-  tablesGroup.x = _CX - tablesGroup.width/2
+  local rectOption1 = showOptions(1)
+  local rectOption2 = showOptions(2)
+  local rectOption3 = showOptions(3)
+  local rectOption4 = showOptions(4)
+
+  local rectOptions = {rectOption1, rectOption2, rectOption3, rectOption4}
+  local randomColors = {randomColor1, randomColro2, randomColor3, randomColor4}
+
+  --Proveri koja boja od opcija sadrzi tacnu boju
+  for i = 1, #randomColors do
+    if(trueColorName == randomColors[i]) then
+      score = score + 1
+      rectOptions[i]:addEventListener("tap", SP2)
+    end
+  end
+
+  --Izabrana je pogresna boja
+  for i = 1, #randomColors do
+    if(trueColorName ~= randomColors[i]) then
+      rectOptions[i]:addEventListener("tap", gotoWrongChoice)
+    end
+  end
+
+  progress_bar.x = 0   --ovo je da bi sredina timer-bara bila na levoj ivici
+  progress_bar.y = _CY + 200
+
+  tablesGroup.x = _CX
+  tablesGroup.y = _CY - _CY/2
+
   _grpMain:insert(tablesGroup)
 end
 
@@ -90,24 +252,36 @@ local function gotoSP2()
   SP2()
 end
 
-
 -- Scene events functions
 
 function scene:create(event)
-    print("scene:create - menu")
-    _grpMain = display.newGroup()
+  print("scene:create - menu")
+  _grpMain = display.newGroup()
 
-    self.view:insert(_grpMain)
+  self.view:insert(_grpMain)
 
-    local btnPlay1 = display.newRoundedRect(_grpMain, _CX, _CY-100, 280, 80,20)
-    btnPlay1.fill = {1,1,1}
-    btnPlay1.alpha = 0.4;
+  if utilities:checkBackground() == "white" then
+    background = display.newImageRect(_grpMain, "ColorUpAssets/assets/images/black.png", _W, _H)
+  else
+    background = display.newImageRect(_grpMain, "ColorUpAssets/assets/images/white.png", _W, _H)
+  end
 
-    local lblPlay1 = display.newText("Single player", _CX, _CY-97, "ColorUpAssets/assets/fonts/Galada.ttf", 50)
-    lblPlay1.fill = {0,0,0}
-    _grpMain:insert(lblPlay1)
+  background.x = _CX
+  background.y = _CY
 
-    btnPlay1:addEventListener("tap", gotoSP2)
+  _lblTapToStart = display.newText("Tap to start", _CX, _CY, "assets/fonts/Galada.ttf", 46)
+  _lblTapToStart.fill = theme
+  _grpMain:insert(_lblTapToStart)
+  _lblTapToStart.x = _CX
+  _lblTapToStart.y = _CY
+
+  progress_bar = display.newRect(display.contentCenterX, display.contentCenterY, 2*display.contentWidth, 20)
+  progress_bar:setFillColor(0.2, 0.3, 0.5) -- set the color of the progress bar
+  progress_bar.x = _CX
+  progress_bar.y = _CY + _CY/2
+
+  score = 0  --reset the score when playing the game for the first time or after failure
+  _lblTapToStart:addEventListener("tap", gotoSP2)  
 end
 
 function scene:show(event)
@@ -137,4 +311,3 @@ scene:addEventListener("hide",scene)
 scene:addEventListener("destroy",scene)
     
 return scene
-
---
+++ scenes/wrongChoice.lua
@@ -0,0 +1,115 @@
+-- Import
+
+local composer = require("composer")
+local relayout = require("libs.relayout")
+local utilities = require("classes.utilities")
+
+-- Set variables
+
+--Layout
+local _W, _H, _CX, _CY = relayout._W, relayout._H, relayout._CX, relayout._CY
+
+--Scene
+local scene = composer.newScene()
+
+--Groups
+local _grpMain
+
+--Sounds
+local _click = audio.loadStream("assets/sounds/click.wav")
+
+
+-- Local functions
+local function gotoPlayAgain()
+    utilities:playSound(_click) 
+    composer.gotoScene("scenes.game2")
+    print("scene:create -")
+    _grpMain = display.newGroup()
+end
+
+local function gotoMainMenu()
+    utilities:playSound(_click) 
+    composer.gotoScene("scenes.menu")
+    print("scene:create -")
+    _grpMain = display.newGroup()
+end
+
+
+-- Scene events functions
+
+function scene:create(event)
+    print("scene:create - menu")
+    _grpMain = display.newGroup()
+
+    self.view:insert(_grpMain)
+
+    if utilities:checkBackground() == "white" then
+      background = display.newImageRect(_grpMain, "ColorUpAssets/assets/images/black.png", _W, _H)
+    else
+        background = display.newImageRect(_grpMain, "ColorUpAssets/assets/images/white.png", _W, _H)
+    end
+
+    background.x = _CX
+    background.y = _CY
+
+    --Message
+    _lblMessage = display.newText("Wrong choice!", _CX, _CY, "assets/fonts/Galada.ttf", 46)
+    _lblMessage.fill = theme
+    _grpMain:insert(_lblMessage)
+    _lblMessage.x = _CX
+    _lblMessage.y = _CY
+
+    --Play again
+    local btnPlayAgain = display.newRoundedRect(_grpMain, _CX, _CY-100, 240, 50,20)
+    btnPlayAgain.fill = theme
+    btnPlayAgain.alpha = 0.4;
+
+    local lblPlayAgain = display.newText("Mode 1", _CX, _CY-100, "ColorUpAssets/assets/fonts/alphabetized cassette tapes.ttf", 50)
+    lblPlayAgain.fill = theme
+    _grpMain:insert(lblPlayAgain)
+
+    btnPlayAgain:addEventListener("tap", gotoPlayAgain)
+
+
+    --Main menu
+    local btnMainMenu = display.newRoundedRect(_grpMain, _CX, _CY, 240, 50,20)
+    btnMainMenu.fill = theme
+    btnMainMenu.alpha = 0.4;
+
+    local lblMainMenu = display.newText("Mode 2", _CX, _CY+3, "ColorUpAssets/assets/fonts/alphabetized cassette tapes.ttf", 50)
+    lblMainMenu.fill = theme
+    _grpMain:insert(lblMainMenu)
+
+    btnMainMenu:addEventListener("tap", gotoMainMenu)
+
+
+end
+
+function scene:show(event)
+    if(event.phase == "will") then
+    elseif(event.phase == "did") then 
+    end
+end
+
+function scene:hide(event)
+    if(event.phase == "will") then
+    elseif(event.phase == "did") then 
+    end
+end
+
+function scene:destroy(event)
+    if(event.phase == "will") then
+    elseif(event.phase == "did") then 
+    end
+end
+    
+
+--Scene event listeners
+
+scene:addEventListener("create",scene)
+scene:addEventListener("show",scene)
+scene:addEventListener("hide",scene)
+scene:addEventListener("destroy",scene)
+    
+return scene
+
